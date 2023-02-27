5f1f0cff067167a208f3047a7063ddd071807e45
Max Higgs <mhh41@cam.ac.uk>
2023-02-27 12:40:36 +0000

    - Block picking up updated - return to start/finish area function added - more tests done

--- navigation.ino
+++ navigation.ino
@@ -13,8 +13,8 @@ void follow_line(){
     Serial.println(left_sensor_val);
     Serial.println(right_sensor_val);
     if (left_sensor_val == 1 && right_sensor_val == 1) {
-        right_speed = MOTOR_SPEED_BASE;
-        left_speed = MOTOR_SPEED_BASE;
+        //right_speed = MOTOR_SPEED_BASE;
+        //left_speed = MOTOR_SPEED_BASE;
         //hsin: i recommend we find a way to avoid sending the instruction set_motor_speeds if we are on track to avoid overcrowding the communication channel/sending too many instructions 
     }
     else if (left_sensor_val == 1 && right_sensor_val == 0) {
@@ -66,7 +66,6 @@ void recover_to_line() { //Recovery algorithm for if we lose track of the line
 
 void move_onto_line() { //For when moving out of start box and on to the line
 
-    const int DELAY = 50; //in ms, how much to wait between iterations
     const int MAX_TIME_TO_LINE_1 = 5000; //in ms, how long it is expected to take to get to line (maximum, worry if it takes this long!)
     const int MAX_TIME_TO_LINE_2 = 5000;
     const int MAX_COUNT_1 = MAX_TIME_TO_LINE_1 / DELAY;
@@ -91,7 +90,7 @@ void move_onto_line() { //For when moving out of start box and on to the line
                 line_reached_right = outer_right_sensor_val == 1;
             }
         }
-        if (!line_reached_left && !line_reached_right) set_motor_speeds(-MOTOR_SPEED_BASE, -MOTOR_SPEED_BASE);
+        if (!line_reached_left && !line_reached_right) reverse(WHEELBASE); //Line missed, backtrack a bit
         else part_done = true;
     }
     //Now there is a line to follow, until main line reached
@@ -101,18 +100,20 @@ void move_onto_line() { //For when moving out of start box and on to the line
     set_motor_speeds(MOTOR_SPEED_BASE, MOTOR_SPEED_BASE); //Initial movement
     while (!part_done) {
         int count = 0;
-        while(!line_reached_left && !line_reached_right && MAX_COUNT_2) {
-        follow_line();
-        if (!line_reached_left){
-            int outer_left_sensor_val = get_sensor_reading(LINE_SENSOR_3_PIN);
-            line_reached_left = outer_left_sensor_val == 1;
-        }
-        if (!line_reached_right){
-            int outer_right_sensor_val = get_sensor_reading(LINE_SENSOR_4_PIN);
-            line_reached_right = outer_right_sensor_val == 1;
-        }
+        while(!line_reached_left && !line_reached_right && count < MAX_COUNT_2) {
+            count++;
+            delay(DELAY);
+            follow_line();
+            if (!line_reached_left){
+                int outer_left_sensor_val = get_sensor_reading(LINE_SENSOR_3_PIN);
+                line_reached_left = outer_left_sensor_val == 1;
+            }
+            if (!line_reached_right){
+                int outer_right_sensor_val = get_sensor_reading(LINE_SENSOR_4_PIN);
+                line_reached_right = outer_right_sensor_val == 1;
+            }
         }  
-        if (!line_reached_left && !line_reached_right) set_motor_speeds(-MOTOR_SPEED_BASE, -MOTOR_SPEED_BASE);
+        if (!line_reached_left && !line_reached_right) reverse(WHEELBASE); //Gone too far, bactrack a bit
         else part_done = true;
     }
     //Main line reached, turn right on to line
@@ -123,23 +124,50 @@ void move_onto_line() { //For when moving out of start box and on to the line
 void go_back(){
     //to return to home
     //we are either at fork w green/red box after dropping off block
-    if (fork_count<HOME){
-        follow_line();
-        if (get_sensor_reading(LINE_SENSOR_3_PIN) ^ get_sensor_reading(LINE_SENSOR_4_PIN)){
-            turn(-BLOCK_ROTATION_ANGLE);
-            //some algo to make robot get in the white square 
-            }
-        
+    if (fork_count<HOME){ //at green block
+        forward(WHEELBASE); //So it does not accidentally read the fork for the green block twice
+        while(get_sensor_reading(LINE_SENSOR_4_PIN) != 1) { //While yet to detect home fork
+            follow_line(); //Line following loop
+        }
+    }
+    else if(fork_count>HOME){
+        turn(180); //Spin to face line
+        while (get_sensor_reading(LINE_SENSOR_3_PIN) != 1){ //While yet to detect home fork (on left now)
+            follow_line(); //Line following loop
+        }
     }
 
-    else if(fork_count>HOME){
-        reverse(FORK_DISTANCE);
-         if (get_sensor_reading(LINE_SENSOR_3_PIN) ^ get_sensor_reading(LINE_SENSOR_4_PIN)){
-            turn(-BLOCK_ROTATION_ANGLE);
-            //some algo to make robot get in the white square 
-            }
-        
-    }
+    forward(WHEELBASE); //Move forward wheelbase of car (rear axle now over line)
+    turn(-90); //Turn to face box
+    
+    //Now line to follow, until edge of box detected
+    bool line_reached_left = true;
+    bool line_reached_right = true;
+    bool done = false;
+
+    const int MAX_TIME_TO_LINE = 5000;
+    const int MAX_COUNT = MAX_TIME_TO_LINE / DELAY;
+
+    set_motor_speeds(MOTOR_SPEED_BASE, MOTOR_SPEED_BASE); //Initial movement
+    int count = 0;
+    while(!line_reached_left && !line_reached_right && count < MAX_COUNT) { 
+        count++;
+        delay(DELAY);
+        follow_line();
+        if (!line_reached_left){
+            int outer_left_sensor_val = get_sensor_reading(LINE_SENSOR_3_PIN);
+            line_reached_left = outer_left_sensor_val == 1;
+        }
+        if (!line_reached_right){
+            int outer_right_sensor_val = get_sensor_reading(LINE_SENSOR_4_PIN);
+            line_reached_right = outer_right_sensor_val == 1;
+        }
+    }  
+    //If maximum count reached, stop and hope you're in the box (this may be changed on testing)
+    forward(WHEELBASE); //move forward a bit to make sure you're completely in the box
+
+
+
     
         
         
